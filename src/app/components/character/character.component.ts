291a9bd44de54ca9d0c627db864e60b946bb2a26
Facundo Arambillet <Facundoarambillet@gmail.com>
2023-08-02 21:26:25 -0300

    Adding attributes on characters to display in model

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -11,9 +11,40 @@ export class CharacterComponent {
 
   image: string = '';
   name: string = '';
+  description: string = '';
+  comics : string[] = [];
+  events : string[] = [];
+  series : string[] = [];
+  stories : string[] = [];
+
+  loadComics() {
+    for(let comic of this.character.comics.items) {
+      this.comics.push(comic.name);
+    }
+  }
+  loadEvents() {
+    for(let event of this.character.events.items) {
+      this.events.push(event.name);
+    }
+  }
+  loadSeries() {
+    for(let serie of this.character.series.items) {
+      this.series.push(serie.name);
+    }
+  }
+  loadStories() {
+    for(let storie of this.character.stories.items) {
+      this.stories.push(storie.name);
+    }
+  }
 
   ngOnInit() {
     this.image = this.character.thumbnail.path + '.' + this.character.thumbnail.extension;
     this.name = this.character.name;
+    this.description = this.character.description
+    this.loadComics();
+    this.loadEvents();
+    this.loadSeries();
+    this.loadStories();
   }
 }
